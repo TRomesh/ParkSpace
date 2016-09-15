7ba99177e4008be4d1290a96375fedf831a689d9
TRomesh <tharakaromesh@gmail.com>
2016-09-15 06:38:52 +0530

    added bower along with bootstrap and jquery

---
+++ src/app/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {render} from "react-dom";
+
+import {Home} from "./components/Home";
+
+class App extends React.Component{
+    render(){
+      return (
+       <div>
+         <Home/>
+       </div>
+     );
+   }
+}
+
+render(<App />, window.document.getElementById('app'));
