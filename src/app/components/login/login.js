e88f7f3e7361466b40b84a4100867ff609b0a4c8
TRomesh <tharakaromesh@gmail.com>
2016-10-09 01:04:55 +0530

    adding components and images

---
+++ src/app/components/login/login.js
@@ -0,0 +1,27 @@
+import React,{PropTypes} from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import {Link} from 'react-router';
+import {connect} from 'react-redux';
+
+
+class Login extends React.Component{
+
+  constructor(props,contex){
+     super(props,contex);
+
+  }
+
+  render(){
+    return(
+       <div>lodfdfdsdsddf</div>
+    );
+  }
+
+
+}
+
+
+
+
+export default Login;
