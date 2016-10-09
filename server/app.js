cdd428c665be26573dcda9f6cf8e066eb121f719
TRomesh <tharakaromesh@gmail.com>
2016-10-09 16:47:39 +0530

    adding mongoose

--- server/app.js
+++ server/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
-
 const router = express.Router();
+const bodyParser = require('body-parser');
+const path = require('path');
+
 
 app.use('/', express.static('src'));
 
-router.get('/sample', (req, res) => {
+router.get('/register', (req, res) => {
+
+});
+
+router.get('/park', (req, res) => {
 
 });
 
