024f7789414266930df690abd4a106096472dbbb
TRomesh <tharakaromesh@gmail.com>
2016-10-09 18:30:14 +0530

    adding routes files

---
+++ server/routes/parking.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+
+const parking = function(router, bodyParser, mongoose) {
+	router.get('', bodyParser.json(), function (req, res) {
+
+	})
+
+	.post('', bodyParser.json(), function(req, res) {
+
+	});
+
+}
+
+module.exports = parking;
---
+++ server/routes/userRegister.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+
+const userRegister = function(router, bodyParser, mongoose) {
+	router.get('', bodyParser.json(), function (req, res) {
+
+	})
+
+	.post('', bodyParser.json(), function(req, res) {
+
+	});
+
+}
+
+module.exports = userRegister;
