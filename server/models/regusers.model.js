9b19cd5b1bd97aac3a02bcddb20a0907b4429f58
TRomesh <tharakaromesh@gmail.com>
2016-12-31 07:42:28 +0530

    fix hasing issue bcrypt

--- server/models/regusers.model.js
+++ server/models/regusers.model.js
@@ -11,5 +11,24 @@ const regUsers = new Schema({
   password:String
 });
 
+//On save Hook,encrypt password
+//Before saving a model, run this function
+regUsers.pre('save',function (next){
+    // get access to this user model
+    let user = this;
+    console.log(user);
+    // generate salt the run call back
+    bcrypt.genSalt(10,(err,salt)=>{
+       if(err){ return next(err);}
+      // hash(encrypt) our password using salt
+       bcrypt.hash(user.password,salt,null,(err,hash)=>{
+            if(err){ return next(err);}
+            //overwrite plain text password with encrypted password
+            user.password = hash;
+            next();
+       });
+    })
+});
+
 
 module.exports = mongoose.model('regusers',regUsers);
